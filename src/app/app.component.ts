64af7e35d19d4771164e56dd749d89d4c9d74dd1
Abdou <abdourahamane.ly1@gmail.com>
2018-01-14 14:56:11 +0100

    ajout d'un loading et des animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import {Router} from '@angular/router';
+import {routerTransition} from './core/animations/router.transition';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations: [routerTransition]
+
 })
 export class AppComponent {
 
   app = 'title';
 
   constructor(router: Router) {
-    //router.navigateByUrl('/form');
+    // router.navigateByUrl('/form');
   }
 }
